75f2059454b3ecbea321767b20c12b6771a3b1dc
romeobluesky <romeobluesky@gmail.com>
2025-10-08 16:06:25 +0900

    consultant_status 상태 추가 06

--- routes/consultants.js
+++ routes/consultants.js
@@ -797,13 +797,14 @@ router.put('/:id/status', authenticateToken, validateId, async (req, res) => {
 
     // 본인 확인 (상담사 본인이거나 관리자만 수정 가능)
     // role_level: 8 = 상담사, 10 = 관리자
+    // consultants.user_id는 users.login_id를 참조
     const isConsultantRole = req.user.role_level === 8;
     const isAdmin = req.user.role_level === 10;
-    const isOwner = req.user.id === consultant.user_id;
+    const isOwner = req.user.login_id === consultant.user_id;
 
     // 디버깅 로그
     console.log('=== 권한 체크 디버깅 ===');
-    console.log('req.user.id:', req.user.id);
+    console.log('req.user.login_id:', req.user.login_id);
     console.log('req.user.role_level:', req.user.role_level);
     console.log('consultant.user_id:', consultant.user_id);
     console.log('isConsultantRole:', isConsultantRole);
